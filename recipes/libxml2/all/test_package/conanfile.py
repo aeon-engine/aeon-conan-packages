da2248913e58a3837c3757e3bc2f0eeb276b267a
Robin Degen <robindegen@gmail.com>
2021-06-26 11:49:19 +0200

    Add libxml2

---
+++ recipes/libxml2/all/test_package/conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile, CMake
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "cmake_find_package"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        bin_path = os.path.join("bin", "test_package")
+        xml_path = os.path.join(self.source_folder, "books.xml")
+        bin_arg_path = "%s %s" % (bin_path, xml_path)
+        self.run(bin_arg_path, run_environment=True)
