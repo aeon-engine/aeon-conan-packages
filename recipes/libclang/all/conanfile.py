7578263685bb9d19770badbe33f9f1bb8c05df62
Robin Degen <robindegen@gmail.com>
2021-06-26 11:49:19 +0200

    Fix libclang recipe to use aeon packages

--- recipes/libclang/all/conanfile.py
+++ recipes/libclang/all/conanfile.py
@@ -184,11 +184,12 @@ class LibClangConan(ConanFile):
 
     def requirements(self):
         if self.options.with_ffi:
-            self.requires('libffi/3.3')
+            raise ConanInvalidConfiguration('Building with ffi is not supported.')
+            #self.requires('libffi/3.3')
         if self.options.get_safe('with_zlib', False):
-            self.requires('zlib/1.2.11')
+            self.requires('zlib/1.2.11@aeon/stable')
         if self.options.get_safe('with_xml2', False):
-            self.requires('libxml2/2.9.10')
+            self.requires('libxml2/2.9.12@aeon/stable')
 
     def configure(self):
         if self.options.shared:  # Shared builds disabled just due to the CI
