d54705832974f72a70f4e3e0f3ab27d19c834c28
Robin Degen <robindegen@gmail.com>
2021-06-26 11:49:17 +0200

    Add autoconf

---
+++ recipes/autoconf/all/test_package/conanfile.py
@@ -0,0 +1,40 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from contextlib import contextmanager
+import os
+import shutil
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    exports_sources = "configure.ac", "config.h.in", "Makefile.in", "test_package_c.c", "test_package_cpp.cpp",
+
+    def build_requirements(self):
+        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/20210604@aeon/stable")
+
+    @contextmanager
+    def _build_context(self):
+        if self.settings.compiler == "Visual Studio":
+            with tools.vcvars(self.settings):
+                with tools.environment_append({"CC": "cl -nologo", "CXX": "cl -nologo",}):
+                    yield
+        else:
+            yield
+
+    def build(self):
+        # https://github.com/conan-io/conan/issues/8883
+        pass
+        #for src in self.exports_sources:
+        #    shutil.copy(os.path.join(self.source_folder, src), self.build_folder)
+        #self.run("{} --verbose".format(os.environ["AUTOCONF"]), win_bash=tools.os_info.is_windows)
+        #self.run("{} --help".format(os.path.join(self.build_folder, "configure").replace("\\", "/")), win_bash=tools.os_info.is_windows)
+        #autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        #with self._build_context():
+        #    autotools.configure()
+        #    autotools.make()
+
+    def test(self):
+        # https://github.com/conan-io/conan/issues/8883
+        pass
+        #if not tools.cross_building(self.settings):
+        #    self.run(os.path.join(".", "test_package"), run_environment=True)
