02b70a2f556b9e279a08f83d55598dc827a06268
Robin Degen <robindegen@gmail.com>
2021-07-01 23:26:59 +0200

    Add powershell build script

---
+++ build_package.ps1
@@ -0,0 +1,69 @@
+function Prepare
+{
+    if (Test-Path "version.txt")
+    {
+        Remove-Item "version.txt"
+    }
+
+    conan remote add aeon ${env:conan_remote_url}
+
+    $result = $LASTEXITCODE
+    if ($result -ne 0)
+    {
+        throw "Failed to add conan remote: $result"
+    }
+
+    conan remote remove conancenter
+
+    $result = $LASTEXITCODE
+    if ($result -ne 0)
+    {
+        throw "Conan remove conancenter failed: $result"
+    }
+
+    conan remote remove conan-center
+
+    $result = $LASTEXITCODE
+    if ($result -ne 0)
+    {
+        throw "Conan remove conan-center failed: $result"
+    }
+
+    conan user -p ${env:conan_api_key} -r aeon ${env:conan_api_user}
+
+    $result = $LASTEXITCODE
+    if ($result -ne 0)
+    {
+        throw "Failed to set conan credentials: $result"
+    }
+
+    $Env:CONAN_CPU_COUNT=4
+}
+
+function Build
+{
+    python build_package.py ${env:package_name}/${env:package_version}@${env:user_channel} -s build_type=${env:build_type}
+    $result = $LASTEXITCODE
+    if ($result -ne 0)
+    {
+        throw "Conan failed with exit code $result"
+    }
+}
+
+function Deploy
+{
+    $package_version = [IO.File]::ReadAllText("version.txt")
+    echo "Deploying $package_version"
+
+    conan upload $package_version --all -r=aeon
+
+    $result = $LASTEXITCODE
+    if ($result -ne 0)
+    {
+        throw "Failed to set upload package: $result"
+    }
+}
+
+Prepare
+Build
+Deploy
