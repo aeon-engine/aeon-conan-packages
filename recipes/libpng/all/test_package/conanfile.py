ff3919a7ee9e6a692b27f05a7c197abfec439304
Robin Degen <robindegen@gmail.com>
2021-07-04 15:17:55 +0200

    Fix libpng package testing on arm; there is no reason to not just run the regular test

--- recipes/libpng/all/test_package/conanfile.py
+++ recipes/libpng/all/test_package/conanfile.py
@@ -17,14 +17,5 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            if "arm" in self.settings.arch and not tools.os_info.is_macos:
-                self.test_arm()
-            else:
-                bin_path = os.path.join("bin", "test_package")
-                self.run(bin_path, run_environment=True)
-
-    def test_arm(self):
-        file_ext = "so" if self.options["libpng"].shared else "a"
-        lib_path = os.path.join(self.deps_cpp_info["libpng"].libdirs[0], "libpng.%s" % file_ext)
-        output = subprocess.check_output(["readelf", "-h", lib_path]).decode()
-        assert re.search(r"Machine:\s+ARM", output)
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
