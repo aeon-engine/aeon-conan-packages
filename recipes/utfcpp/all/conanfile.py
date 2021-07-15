58b35bdbec30cdb111df2f8e9deb34d3d873cf2f
Robin Degen <robindegen@gmail.com>
2021-07-15 18:20:02 +0200

    Add utfcpp

---
+++ recipes/utfcpp/all/conanfile.py
@@ -0,0 +1,67 @@
+from conans import ConanFile, tools
+import os
+import textwrap
+
+required_conan_version = ">=1.33.0"
+
+
+class UtfCppConan(ConanFile):
+    name = "utfcpp"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/nemtrif/utfcpp"
+    description = "UTF-8 with C++ in a Portable Way"
+    settings = "os", "compiler", "arch", "build_type"
+    topics = ("utf", "utf8", "unicode", "text")
+    license = "BSL-1.0"
+    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def package_id(self):
+        self.info.header_only()
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+    def package(self):
+        self.copy("*.h",
+                  dst=os.path.join("include", "utf8cpp"),
+                  src=os.path.join(self._source_subfolder, "source"))
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+
+        self._create_cmake_module_alias_targets(
+            os.path.join(self.package_folder, self._module_file_rel_path),
+            {"utf8cpp": "utf8cpp::utf8cpp"}
+        )
+
+    @staticmethod
+    def _create_cmake_module_alias_targets(module_file, targets):
+        content = ""
+        for alias, aliased in targets.items():
+            content += textwrap.dedent("""\
+                if(TARGET {aliased} AND NOT TARGET {alias})
+                    add_library({alias} INTERFACE IMPORTED)
+                    set_property(TARGET {alias} PROPERTY INTERFACE_LINK_LIBRARIES {aliased})
+                endif()
+            """.format(alias=alias, aliased=aliased))
+        tools.save(module_file, content)
+
+    @property
+    def _module_subfolder(self):
+        return os.path.join("lib", "cmake")
+
+    @property
+    def _module_file_rel_path(self):
+        return os.path.join(self._module_subfolder,
+                            "conan-official-{}-targets.cmake".format(self.name))
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "utf8cpp"
+        self.cpp_info.names["cmake_find_package_multi"] = "utf8cpp"
+        self.cpp_info.builddirs.append(self._module_subfolder)
+        self.cpp_info.build_modules["cmake_find_package"] = [self._module_file_rel_path]
+        self.cpp_info.build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
+        self.cpp_info.includedirs.append(os.path.join("include", "utf8cpp"))
